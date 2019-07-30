790271c1fc4b77dc1e5c6a1c1d82d482e5984fc0
UnicornBear <46576621+UnicornBear@users.noreply.github.com>
2019-07-29 21:06:40 -0600

    Update Jumbotron.js

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
 <div className="jumbotron jumbotron-fluid">
   <div className="container text-center">
-    <h1 className="display-4">React Clicky Game!</h1>
+    <h1 className="display-4">NHL React Clicky Game!</h1>
     <p className="lead">Click on any image to earn a point, but don't click on any picture more than once!</p>
   </div>
 </div>
