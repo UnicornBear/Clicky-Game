5eff7b3b1443b825508d7f51e315cd0489c56c63
UnicornBear <jtbregenzer@gmail.com>
2019-08-03 09:56:55 -0600

    Updates

---
+++ precache-manifest.680f1d9853bb8159c1531d79d8bb8e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db25ac68bb6ba95c9a8adb2c2a7a1468",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f1b27fc13e038c3309e3",
+    "url": "/clicky-game/static/css/main.9f4308ad.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f1b27fc13e038c3309e3",
+    "url": "/clicky-game/static/js/main.e0c8b501.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
